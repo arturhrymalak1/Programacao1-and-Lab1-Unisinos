94c5d8cab774384d8a665f383ba3e913b46e12ca
Artur Hrymalak Llantada <arturllantada@outlook.com>
2025-08-25 13:10:53 -0300

    exercicios adicionados

---
+++ exercicios_semana_3_e_4/exercicio_11.py
@@ -0,0 +1,13 @@
+
+numeros = []
+
+i = 1
+
+while i <= 10:
+    x = int(input("Digite um numero: "))
+    i += 1
+    
+    numeros.append(x)
+
+
+print(numeros)
---
+++ exercicios_semana_3_e_4/exercicio_9.py
@@ -0,0 +1,11 @@
+
+def contarPares(i):
+    while True:
+        print(i)
+        i += 2
+        if i == 2000:
+            break
+
+b = contarPares(0)
+
+print(b)
