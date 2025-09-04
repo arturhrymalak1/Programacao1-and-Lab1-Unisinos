85ae5c63fa80f8b52ac739f0e264a8c04c9909bf
Artur Hrymalak Llantada <arturllantada@outlook.com>
2025-09-04 13:07:59 -0300

    Inicio de Classe

---
+++ JAVA_NOTES/ExerciciosJ_semana_5-6/Cores.java
@@ -0,0 +1,32 @@
+public class Cores{
+
+    private int r;
+    private int g;
+    private int b;
+
+    public Cores(int r, int g, int b){
+
+    this.r = r;
+    this.g = g;
+    this.b = b;
+
+    };
+
+    public int setR(){
+        return r;
+    
+    }
+
+    public int getR(){
+        return this.r;
+    }
+
+    public int getG(){
+        return this.g;
+    }
+
+    public int getB(){
+        return this.b;
+    }
+
+}
